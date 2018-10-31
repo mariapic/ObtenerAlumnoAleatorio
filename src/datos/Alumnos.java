fa90504674cfba5e7c0a714b89140af962aae020
6070 <6070@SHCZC64171Z1.uem.es>
2018-10-31 13:12:33 +0100

    Actualización

--- src/datos/Alumnos.java
+++ src/datos/Alumnos.java
@@ -1,34 +1,53 @@
 package datos;
 
 public class Alumnos {
-	public String [] arrayAlumnos = {
-			"ALONSO CASILDA, Daniel",
-			"Álvarez, Javier",
-			"ARGUDO RIVERA, Santiago",
-			"BARRERO JIMÉNEZ, Sergio",
-			"CUBERO RINCÓN, Mario",
-			"FERNÁNDEZ DÍEZ, Carlos",
-			"FERNÁNDEZ RAMÍREZ, Luis",
-			"García Herrero, Germán",
-			"GARCÍA MARTÍN, Jorge",
-			"GONZÁLEZ LÓPEZ, Marco",
-			"GONZÁLEZ MARTÍN, Sergio",
-			"GORDO SABÍN, Borja",
-			"Gutierrez Bermudez, Ricardo",
-			"IGLESIAS DÍAZ, Yago",
-			"Juiña, Juan",
-			"LENIS CORTES, Juan Camilo",
-			"LONDRES BLANES, Martín",
-			"LÓPEZ JIMÉNEZ, Ignacio",
-			"MARTÍNEZ CABRERA, Nicolás",
-			"MICHEL SÁNCHEZ, Álvaro",
-			"MOLINA PEDRAZA, Juan Carlos",
-			"MORENO CASTRO, Jorge",
-			"MORENO TOLEDANO, Juan",
-			"MUÑOZ HIDALGO, Andrea",
-			"PARDOS SEVILLA, Carlos",
-			"SÁNCHEZ BORREGO, Daniel",
-			"SEGURA GÓMEZ, Andrés",
-			"SOTOMAYOR NAVARRETE, José María",
-	};
+	private String grupo;
+	private String[] arrayAlumnos;
+
+	public Alumnos(String grupo) {
+		cargarGrupo(grupo);
+	}
+
+	private void cargarGrupo(String grupo) {
+		if (grupo.equals("M11")) {
+			arrayAlumnos = new String[28];
+
+			arrayAlumnos[0] = "ALONSO CASILDA, Daniel";
+			arrayAlumnos[1] = "Álvarez, Javier";
+			arrayAlumnos[2] = "ARGUDO RIVERA, Santiago";
+			arrayAlumnos[3] = "BARRERO JIMÉNEZ, Sergio";
+			arrayAlumnos[4] = "CUBERO RINCÓN, Mario";
+			arrayAlumnos[5] = "FERNÁNDEZ DÍEZ, Carlos";
+			arrayAlumnos[6] = "FERNÁNDEZ RAMÍREZ, Luis";
+			arrayAlumnos[7] = "García Herrero, Germán";
+			arrayAlumnos[8] = "GARCÍA MARTÍN, Jorge";
+			arrayAlumnos[9] = "GONZÁLEZ LÓPEZ, Marco";
+			arrayAlumnos[10] = "GONZÁLEZ MARTÍN, Sergio";
+			arrayAlumnos[11] = "GORDO SABÍN, Borja";
+			arrayAlumnos[12] = "Gutierrez Bermudez, Ricardo";
+			arrayAlumnos[13] = "IGLESIAS DÍAZ, Yago";
+			arrayAlumnos[14] = "Juiña, Juan";
+			arrayAlumnos[15] = "LENIS CORTES, Juan Camilo";
+			arrayAlumnos[16] = "LONDRES BLANES, Martín";
+			arrayAlumnos[17] = "LÓPEZ JIMÉNEZ, Ignacio";
+			arrayAlumnos[18] = "MARTÍNEZ CABRERA, Nicolás";
+			arrayAlumnos[19] = "MICHEL SÁNCHEZ, Álvaro";
+			arrayAlumnos[20] = "MOLINA PEDRAZA, Juan Carlos";
+			arrayAlumnos[21] = "MORENO CASTRO, Jorge";
+			arrayAlumnos[22] = "MORENO TOLEDANO, Juan";
+			arrayAlumnos[23] = "MUÑOZ HIDALGO, Andrea";
+			arrayAlumnos[24] = "PARDOS SEVILLA, Carlos";
+			arrayAlumnos[25] = "SÁNCHEZ BORREGO, Daniel";
+			arrayAlumnos[26] = "SEGURA GÓMEZ, Andrés";
+			arrayAlumnos[27] = "SOTOMAYOR NAVARRETE, José María";
+		}
+
+	}
+
+	public String[] getArrayAlumnos() {
+		return arrayAlumnos;
+	}
+	
+	
+
 }
--- src/main/GeneradorAlumnosAlea.java
+++ src/main/GeneradorAlumnosAlea.java
@@ -1,17 +1,40 @@
 package main;
 
 import java.util.Random;
+import java.util.Scanner;
 
 import datos.Alumnos;
 
 public class GeneradorAlumnosAlea {
 
+	static Random rd;
 	public static void main(String[] args) {
-		Random rd = new Random();
-		Alumnos datos = new Alumnos();
-		int i = rd.nextInt(datos.arrayAlumnos.length);
-		System.out.println(datos.arrayAlumnos[i]);
+		Scanner sc = new Scanner(System.in);
+		rd = new Random();
+		
+		System.out.println("Introduce grupo:");
+		String grupo = sc.nextLine();
+		
+		char resp = 'S';
+		while (resp != 'N') {
+			System.out.println("Desea obtener un alumno de forma aleatoria?(s/n)");
+			resp = sc.nextLine().toUpperCase().charAt(0);
+			
+			if (resp == 'S') {
+				mostrarAlumno(grupo);
+			}
+		}
+		
+		sc.close();
+		
 
 	}
 
+	private static void mostrarAlumno(String grupo) {
+		Alumnos datos = new Alumnos(grupo);
+		int i = rd.nextInt(datos.getArrayAlumnos().length);
+		System.out.println("*********************************");
+		System.out.println("--> " + datos.getArrayAlumnos()[i]);
+		System.out.println("*********************************");
+	}
 }
